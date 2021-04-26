7310dd37c7ec2dfc3abbe91d06b0eccc24fbeabe
Mateusz Kojro <mateuszkojro@outlook.com>
2021-04-27 00:41:12 +0200

    Adding interface for effect functors

---
+++ Effect.h
@@ -0,0 +1,14 @@
+//
+// Created by mateu on 4/27/2021.
+//
+
+#ifndef VIDEO_PLAYER_EFFECT_H
+#define VIDEO_PLAYER_EFFECT_H
+
+#include <opencv2/core.hpp>
+
+class Eeffect {
+    void operator()(const cv::Mat &frame) = 0;
+};
+
+#endif //VIDEO_PLAYER_EFFECT_H
