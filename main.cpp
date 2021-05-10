ab62365fc2a3822add4e8c008fb65157a4faefc8
Mateusz Kojro <mateuszkojro@outlook.com>
2021-05-10 17:13:16 +0200

    Adding lua to CMake

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
 
 #include "VideoPlayer.h"
+#include "lua.hpp"
+
 
 int main(int argc, char *argv[])
 {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
     QApplication app(argc, argv);
     VideoPlayer window;
     window.show();
