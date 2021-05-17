5f8393a198caa4e0095f1b46fe231b3df37c6bea
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-05-17 19:43:00 +0200

    plans for video_playback read frame functions are in place, idk about them mutex-bros, for now I'll simply play safe

--- VideoPlayback.cpp
+++ VideoPlayback.cpp
@@ -2,4 +2,49 @@
 // Created by mateu on 5/10/2021.
 //
 
+#include <thread>
 #include "VideoPlayback.h"
+
+void VideoPlayback::change_file(const std::string &path) {
+
+    delete video_capture_;
+    video_capture_ = new cv::VideoCapture(path);
+    std::thread(th_frame_reader);
+}
+
+bool VideoPlayback::read_next_frame() {
+
+    cv::Mat one_frame_buffor;
+    {
+        std::lock_guard<std::mutex> lock(video_capture_mutex_);
+        (*video_capture_) >> one_frame_buffor;
+
+    }
+    if (one_frame_buffor.empty()) {
+
+        video_capture_->set(cv::CAP_PROP_POS_FRAMES, 0);
+        /// if frame is empty return false
+        /// it stops everything
+        return false;
+    }
+
+    std::lock_guard<std::mutex> lock(raw_frames_mutex_);
+    raw_frames_.push(new cv::Mat(one_frame_buffor));
+    return true;
+}
+
+void VideoPlayback::th_frame_reader() {
+    while (2 > 1) {
+        {
+            std::lock_guard<std::mutex> lock(raw_frames_mutex_);
+            /// if the buffor is filled with next 300 frames that is 10s video 30fps
+            /// wait a bit, stop, get some help
+            if (raw_frames_.size() > 30 * 10) std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        /// if  read next frame returns false, the file ended
+        /// so we
+        if (!read_next_frame()) return;
+
+    }
+}
--- VideoPlayback.h
+++ VideoPlayback.h
@@ -1,38 +1,79 @@
 //
 // Created by mateu on 5/10/2021.
+// but written by piotr233 @piotr233
 //
 
 #ifndef VIDEO_PLAYER_VIDEOPLAYBACK_H
 #define VIDEO_PLAYER_VIDEOPLAYBACK_H
 
 #include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <QPixmap>
 #include <queue>
+#include "Effect.h"
+
 
 class VideoPlayback {
+    std::mutex video_capture_mutex_;
+    cv::VideoCapture* video_capture_;
+
+
+    std::array<Effect *, 8> effects_;
+
+    std::mutex raw_frames_mutex_;
+    std::queue<cv::Mat*> raw_frames_;
+
+
+    std::mutex analyzed_frames_mutex_;
+    std::queue<QPixmap> analyzed_frames_;
+
+
+
+
+    bool read_next_frame();
+    void th_frame_reader();
+
 public:
     VideoPlayback(const std::string &path);
 
     /// @brief Get the next frame in the queue
     /// @return
-    cv::Mat next_frame();
+    const QPixmap &next_frame();
+
+    // but like yt  so we need to clean our buffor
+    // move in file in reverse
+    // read all of the frames again
+    void change_effect(int index, Effect *effect);
+
+
+    // clean buffor
+    // open new file
+    // read new file
+    void change_file(const std::string &path);
 
 
     /// @brief Jump to specified frame clean queue (maybe not all of it if we want to be fast)
     /// @param index the frame to skip to
     /// @return
-    cv::Mat operator()(int index);
+    void change_position(int index);
+
+    void skip_10s();
+
+    void back_10s();
 
     /// @brief Index of the current frame
     /// @return
     unsigned current_frame();
 
+    // in frames i guess
+    unsigned file_size();
 
-
-private:
-
-    std::queue<cv::Mat> queue_;
+    /// Later poza kolejka
+    cv::Mat get_frame_raw(int index);
 
 };
+
+
 
 
 #endif //VIDEO_PLAYER_VIDEOPLAYBACK_H
